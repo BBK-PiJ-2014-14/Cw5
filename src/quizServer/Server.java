5287979e201060268a75ef4531018a000401eeb5
BBK-PiJ-2014-14 <skbigmac@hotmail.com>
2015-02-21 17:50:07 +0000

    Server Implementation
    Server class has written, test passed

--- src/quizServer/Server.java
+++ src/quizServer/Server.java
@@ -1,13 +1,39 @@
 package quizServer;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+
+/**
+ * 
+ * @author Noam
+ * This is the class of the server.
+ * This server upload 3 remote objects: QuestionImpl, QuizImpl, and QuizerImpl.
+ */
 public class Server {
-
+	
+	/**
+     * Main method of the server, turn on the server.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
-
+		Server server = new Server();
+		server.serverRegister();
 	}
+	
+	/**
+	 * this method register the server and upload it's remote objects.
+	 */
 	public void serverRegister() {
-		
+		try {
+			Registry reg = LocateRegistry.createRegistry(1099);
+			reg.bind("QuizerImpl", new QuizerImpl());
+			reg.bind("QuestionImpl", new QuestionImpl());
+			reg.bind("QuizImpl", new QuizImpl());
+		} catch (RemoteException | AlreadyBoundException e) {
+			e.printStackTrace();
+		}	
 	}
-
 }
